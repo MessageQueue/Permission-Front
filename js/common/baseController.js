c0acb5b871abe37de6f6f11920d4589af308395e
huiliang <belongs_mymusic@163.com>
2018-08-23 18:10:14 +0800

    add

--- js/common/baseController.js
+++ js/common/baseController.js
@@ -1,104 +1,157 @@
 app.controller("baseController", function ($scope) {
-    //选中的元素
+
+    /**
+     *
+     * 复选框中的ID
+     *
+     * */
     $scope.selectIds = [];
+
+
+    /**
+     *
+     * 分页查找中的实体
+     *
+     * **/
     $scope.entitys = [];
 
-    //新增窗口相关配置
+
+    /**
+     *
+     * 新增实体
+     *
+     * */
+    $scope.entity = {};
+
+
+    /***
+     *
+     * 新增窗口配置
+     *
+     *
+     * */
     $scope.windownInfo = {
         title: "",
         url: ""
     };
 
 
+    /**
+     *
+     * 是否是更新操作
+     *
+     * */
+    $scope.isUpdate = false;
+
+
+    /**
+     *
+     * 分页插件配置
+     *
+     * **/
     $scope.paginationConf = {
         currentPage: 1,
-        totalItems: 8000,
-        itemsPerPage: 15,
+        totalItems: 10,
+        itemsPerPage: 10,
         pagesLength: 10,
-        perPageOptions: [5, 10, 15, 20],
+        perPageOptions: [10, 20, 30],
         onChange: function () {
-            // var nowPage = $scope.paginationConf.currentPage;
-            // var pageSize = $scope.paginationConf.pagesLength;
-            // $scope.findPage(nowPage, pageSize);
             $scope.refreshPage();
         }
     };
 
 
+    /**
+     *
+     * 刷新
+     *
+     * **/
     $scope.refreshPage = function () {
-        console.log("bbbb");
-        $scope.findPage($scope.paginationConf.currentPage, $scope.paginationConf.pagesLength);
-        console.log("cccc");
-
+        $scope.findPage($scope.paginationConf.currentPage, 10);
     };
 
 
-    //表格相关配置
-    $scope.pageInfo = {
-        options: {
-            icons: {
-                refresh: 'glyphicon-refresh icon-refresh',
-                toggle: 'glyphicon-list-alt',
-                columns: 'glyphicon-list'
-            },
-            showRefresh: true,
-            striped: true,
-            iconSize: 'outline',
-            search: true,
-            cache: false,
-            showColumns: true,
-            checkboxHeader: false,
-            showHeader: true,
-            sidePagination: "server",
-            toolbar: "#toolbar",
-            searchOnEnterKey: true,
-            pagination: true,
-            pageNumber: 1,
-            pageSize: 10,
-            pageList: [5, 10, 20, 50],
-            selected: [],
-            minimumCountColumns: 3,
-            onCheck: function (row) {
-                $scope.selectIds.push(row.id);
-                $scope.$apply();
-            },
-            onUncheck: function (row) {
-                var index = $scope.selectIds.indexOf(row.id);
-                $scope.selectIds.splice(index, 1);
-                $scope.$apply();
-            },
-            onRefresh: function () {
-                console.log("cccc");
-                // $scope.findAll(param);
-                // $scope.findPage($scope.pageInfo.options.pageNumber, $scope.pageInfo.options.pageSize);
-            },
-            ajax: function (param) {
-                $scope.findAll(param);
-            }
-        }
-    };
-    $scope.addItem = function () {
+    /**
+     *
+     * 新增实体界面
+     *
+     * **/
+    $scope.modalInsertUi = function () {
         layer.open({
             type: 2,
             title: $scope.windownInfo.title,
             area: ['830px', '450px'], //宽高
             fix: false, //不固定
             maxmin: true,
+            skin: 'demo-class',
             content: $scope.windownInfo.url
         });
     };
-    $scope.deleteItem = function () {
-        if ($scope.selectIds.length === 0) {
-            swal({title: "温馨提示", text: "请选择表格中的某一条记录"})
-            return;
-        }
-        $scope.delete();
-    };
-    $scope.editItem = function () {
+
+
+    /**
+     *
+     * 删除实体
+     *
+     * **/
+    $scope.deleteEntity = function () {
         if ($scope.selectIds.length === 0) {
             swal({title: "温馨提示", text: "请选择表格中的某一条记录"});
+            return;
         }
+        swal({
+            title: "您确定要删除吗",
+            text: "删除后将无法恢复，请谨慎操作！",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#1BB495",
+            confirmButtonText: "删除",
+            cancelButtonText: "取消",
+            closeOnConfirm: false
+        }, function () {
+            $scope.delete();
+            swal({
+                title: "删除成功!",
+                text: "您已经永久删除了这条信息。",
+                type: "success",
+                timer: 500
+            });
+        });
     };
+
+
+    /***
+     *
+     * 编辑实体
+     *
+     * */
+    $scope.updateEntity = function () {
+        if ($scope.selectIds.length === 0) {
+            swal({title: "温馨提示", text: "请选择表格中的某一条记录"});
+            return;
+        }
+        if ($scope.selectIds.length >= 2) {
+            swal({title: "温馨提示", text: "编辑的时候,只能对单行数据进行编辑"});
+            return;
+        }
+        // var params = 111;
+        layer.open({
+            type: 2,
+            title: $scope.windownInfo.title,
+            area: ['830px', '450px'], //宽高
+            fix: false, //不固定
+            maxmin: true,
+            skin: 'demo-class',
+            content: $scope.windownInfo.url + "?id=" + $scope.selectIds[0],
+        });
+    };
+
+
+    /**
+     *
+     * 关闭窗口
+     *
+     * */
     $scope.closeDialog = function () {
         parent.layer.closeAll();
     };
@@ -122,14 +175,19 @@ app.controller("baseController", function ($scope) {
 
     /**
      *
-     *
-     * 获取当前第几页
-     *
+     * 判断insert
      *
      * */
-    $scope.getCurrentPageIndex = function (offset) {
-        var current = (offset / $scope.pageInfo.options.pageSize) + 1;
-        return current;
+    $scope.judgeInsertOrUpdate = function () {
+        var param = getQueryString("id");
+        if (param.length !== 0) {
+            $scope.findOneEntity(param);
+            $scope.isUpdate = true;
+        } else {
+            $scope.isUpdate = false;
+        }
+
     }
+
 
 });
--- js/controller/ResourceController.js
+++ js/controller/ResourceController.js
@@ -1,89 +1,17 @@
 app.controller("ResourceController", function ($scope, $controller, IResourceService) {
 
-
     $controller("baseController", {$scope: $scope});
-
-    $scope.pageInfo.options.columns = [{
-        checkbox: true
-    },
-        {
-            field: 'id',
-            title: 'id',
-            sortable: true,
-            align: "center"
-        },
-        {
-            field: 'p_id',
-            title: '上级资源',
-            sortable: true,
-            align: "center"
-        },
-        {
-            field: 'name',
-            title: '资源名称',
-            sortable: true,
-            align: "center"
-        },
-        {
-            field: 'url',
-            title: '资源地址',
-            sortable: true,
-            align: "center"
-        },
-        {
-            field: 'status',
-            title: '资源状态',
-            sortable: true,
-            align: "center"
-        },
-        {
-            field: 'create_date',
-            title: '创建时间',
-            sortable: true,
-            align: "center"
-        },
-        {
-            field: 'update_date',
-            title: '修改时间',
-            sortable: true,
-            align: "center"
-        },
-    ];
-
-
     $scope.windownInfo.title = "新增资源";
     $scope.windownInfo.url = "../../views/resource/resource-add.html";
+    $scope.searchEntity = {};
 
 
-    //读取列表数据绑定到表单中
-    $scope.findAll = function (params) {
-        console.log($scope.pageInfo);
-        var index = $scope.getCurrentPageIndex(params.data.offset);
-        IResourceService.findPage(index, $scope.pageInfo.options.pageSize).then(function (response) {
-            // params.success({
-            //     total: response.data.data.total,
-            //     rows: response.data.data.records,
-            //     pageNumber: response.data.data.current
-            //
-            // });
-        });
-    };
-
-    $scope.findAll = function (page, rows) {
-        // console.log($scope.pageInfo);
-        // var index = $scope.getCurrentPageIndex(params.data.offset);
-        IResourceService.findPage(index, $scope.pageInfo.options.pageSize).then(function (response) {
-            // params.success({
-            //     total: response.data.data.total,
-            //     rows: response.data.data.records,
-            //     pageNumber: response.data.data.current
-            //
-            // });
-        });
-    };
-
-
-    //分页
+    /**
+     *
+     * 获取分页数据
+     *
+     *
+     * **/
     $scope.findPage = function (page, rows) {
         IResourceService.findPage(page, rows).then(function (response) {
             $scope.entitys = response.data.data.records;
@@ -93,37 +21,50 @@ app.controller("ResourceController", function ($scope, $controller, IResourceSer
     };
 
 
-    //查询实体
-    $scope.findOne = function (id) {
-        IResourceService.findOne(id).success(
-            function (response) {
-                $scope.entity = response.data;
+    /**
+     *
+     * 获取单一实体
+     *
+     * */
+    $scope.findOneEntity = function (id) {
+        IResourceService.findOne(id).then(function (response) {
+            if (response.data.code === 1000) {
+                $scope.entity = response.data.data;
             }
-        );
+        });
     };
 
-    //保存
-    $scope.save = function () {
-        var serviceObject;//服务层对象
+
+    /**
+     *
+     *
+     * 保存或则更改实体
+     *
+     * **/
+    $scope.saveOrUpdateEntity = function () {
+        var serviceObject;
         if ($scope.entity.id != null) {
-            serviceObject = IResourceService.update($scope.entity); //修改
+            serviceObject = IResourceService.update($scope.entity);
         } else {
-            serviceObject = IResourceService.add($scope.entity);//增加
+            serviceObject = IResourceService.add($scope.entity);
         }
         serviceObject.then(function (response) {
             if (response.data.code === 1000) {
-                $scope.refreshPage();
+                $scope.closeDialog();
+                window.parent.document.getElementById("refresh").click();
             }
         });
-        $scope.closeDialog();
-    }
+        $scope.selectIds=[];
+    };
 
 
-    //批量删除
+    /***
+     *
+     *
+     * 删除实体
+     *
+     * */
     $scope.delete = function () {
-        if ($scope.selectIds.length === 0) {
-            return;
-        }
         IResourceService.delete($scope.selectIds).then(function (response) {
             console.log(response);
             if (response.data.code === 1000) {
@@ -133,9 +74,12 @@ app.controller("ResourceController", function ($scope, $controller, IResourceSer
         });
     };
 
-    $scope.searchEntity = {};//定义搜索对象
 
-    //搜索
+    /**
+     *
+     * 查找实体
+     *
+     * */
     $scope.search = function (page, rows) {
         IResourceService.search(page, rows, $scope.searchEntity).success(
             function (response) {
