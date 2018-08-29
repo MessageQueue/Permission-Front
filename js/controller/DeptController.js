94f4f43c1445e7b2997a66967ea27780f65b3a98
huiliang <belongs_mymusic@163.com>
2018-08-29 17:03:29 +0800

    add

---
+++ js/controller/DeptController.js
@@ -0,0 +1,95 @@
+app.controller("DeptController", function ($scope, $controller,IDeptService) {
+
+
+    $controller("baseController", {$scope: $scope});
+    $scope.windownInfo.title = "部门管理";
+    $scope.windownInfo.url="../../views/dept/dept-add.html";
+
+    $scope.searchEntity = {};
+
+
+    /**
+     *
+     * 获取分页数据
+     *
+     *
+     * **/
+        $scope.findPage = function (page, rows) {
+        IDeptService.findPage(page, rows).then(function (response) {
+            $scope.entitys = response.data.data.records;
+            $scope.paginationConf.totalItems = response.data.data.total;
+
+        });
+    };
+
+
+    /**
+     *
+     * 获取单一实体
+     *
+     * */
+        $scope.findOneEntity = function (id) {
+        IDeptService.findOne(id).then(function (response) {
+            if (response.data.code === 1000) {
+                $scope.entity = response.data.data;
+            }
+        });
+    };
+
+
+    /**
+     *
+     *
+     * 保存或则更改实体
+     *
+     * **/
+        $scope.saveOrUpdateEntity = function () {
+        var serviceObject;
+        if ($scope.entity.id != null) {
+            serviceObject = IDeptService.update($scope.entity);
+        } else {
+            serviceObject = IDeptService.add($scope.entity);
+        }
+        serviceObject.then(function (response) {
+            if (response.data.code === 1000) {
+                    $scope.closeDialog();
+                window.parent.document.getElementById("refresh").click();
+            }
+        });
+            $scope.selectIds=[];
+    };
+
+
+    /***
+     *
+     *
+     * 删除实体
+     *
+     * */
+        $scope.delete = function () {
+        IDeptService.delete($scope.selectIds).then(function (response) {
+            console.log(response);
+            if (response.data.code === 1000) {
+                    $scope.refreshPage();
+                $scope.selectIds = [];
+            }
+        });
+    };
+
+
+    /**
+     *
+     * 查找实体
+     *
+     * */
+        $scope.search = function (page, rows) {
+        IDeptService.search(page, rows, $scope.searchEntity).success(
+            function (response) {
+                $scope.list = response.data.records;
+                $scope.paginationConf.totalItems = response.data.total;//更新总记录数
+            }
+        );
+    };
+
+
+});
--- js/service/IResourceService.js
+++ js/service/IResourceService.js
@@ -1,32 +1,32 @@
 //服务层
-app.service('IResourceService', function ($http) {
+app.service('IResourceService',function($http){
 
     //读取列表数据绑定到表单中
-    this.findAll = function () {
+    this.findAll=function(){
         return $http.get('http://localhost:9090/resource/all');
-    };
+    }
     //分页
-    this.findPage = function (page, rows) {
-        return $http.get('http://localhost:9090/resource/list?page=' + page + '&rows=' + rows);
-    };
+    this.findPage=function(page,rows){
+        return $http.get('http://localhost:9090/resource/list?page='+page+'&rows='+rows);
+    }
     //查询实体
-    this.findOne = function (id) {
-        return $http.get('http://localhost:9090/resource/find?id=' + id);
-    };
+    this.findOne=function(id){
+        return $http.get('http://localhost:9090/resource/find?id='+id);
+    }
     //增加
-    this.add = function (entity) {
-        return $http.post('http://localhost:9090/resource/add', entity);
-    };
+    this.add=function(entity){
+        return  $http.post('http://localhost:9090/resource/add',entity );
+    }
     //修改
-    this.update = function (entity) {
-        return $http.post('http://localhost:9090/resource/update', entity);
-    };
+    this.update=function(entity){
+        return  $http.post('http://localhost:9090/resource/update',entity );
+    }
     //删除
-    this.delete = function (ids) {
-        return $http.get('http://localhost:9090/resource/del?ids=' + ids);
-    };
+    this.dele=function(ids){
+        return $http.get('http://localhost:9090/resource/del?ids='+ids);
+    }
     //搜索
-    this.search = function (page, rows, searchEntity) {
-        return $http.post('http://localhost:9090/resource/search.do?page=' + page + "&rows=" + rows, searchEntity);
-    };
+    this.search=function(page,rows,searchEntity){
+        return $http.post('http://localhost:9090/resource/search.do?page='+page+"&rows="+rows, searchEntity);
+    }
 });
